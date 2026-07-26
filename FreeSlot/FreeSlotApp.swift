7cf4b8f2ce9257f7e58182342c28d9e5f1cfeafa
cjt <alice0979528152@gmail.com>
2026-07-26 08:49:49 +0800

    Initial Commit

---
+++ FreeSlot/FreeSlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeSlotApp.swift
+//  FreeSlot
+//
+//  Created by 陳靖婷 on 2026/7/26.
+//
+
+import SwiftUI
+
+@main
+struct FreeSlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
